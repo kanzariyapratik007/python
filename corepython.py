5d0fc4c207273f5b68fc1b108734c8ca17039713
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-03-14 23:31:01 +0530

    Add files via upload
    python data type

---
+++ corepython.py
@@ -0,0 +1,19 @@
+# python data type
+a="pratik"
+print (type(a))#str
+b=(1.888+7)
+print (type(b))#float
+c=1+5j
+print (type(c))#complex
+d=True
+print (type(d))
+e=[1,"pratik",9.999]#list dubliket vlue alow kare
+print (type(e))
+e=(1,"pratik",9.999)#tuple dubliket vlue alow kare
+print (type(e))
+e={1,"pratik",9.999}#set dubliket vlue alow no  kare
+print (type(e))
+f={'age':88}#dict
+print (type(f))
+print (f.get('age'))#key vlue print karva mate
+print (f)
