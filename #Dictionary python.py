c8c7bc6a01f089d2cfd734f125e03ad6c9161f1d
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-03-15 00:49:56 +0530

    Add files via upload
    Python Dictionary

---
+++ #Dictionary python.py
@@ -0,0 +1,18 @@
+#Dictionary python
+a={"id":2,"fast name":"radhe radhe"}
+print (type(a))
+print (a)
+print (a["fast name"])
+a.pop("id")
+print (a)
+print (a.get("id","no data "))
+print (a.clear())
+print (a)
+b={"id":2,"fast name":"radhe radhe"}
+print (b.keys())
+print (b.values())
+print (b.items())
+for key,values in b.items():
+    print (key,values,sep=" = ")
+    b["id"]=50 #update
+    print (b)
