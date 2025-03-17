c28d31298e44b0b177d56ccbd6c7f534ae9dbd5e
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-03-17 23:12:09 +0530

    Add files via upload
    Conditional Statements

---
+++ Conditional Statements.py
@@ -0,0 +1,30 @@
+#if 
+a=50
+b=100
+if b>a :
+    print ("b is a greardhen a")
+    print ("done")
+ 
+#if else
+
+c=100
+d=50
+if d>c :
+   print ("no ")
+else:
+   print ("sorry")
+
+   #if elif else
+
+e=int(input("enter e vlue "))
+f=int(input("enter f vlue "))
+g=int(input("enter g vlue "))
+if (e>f)  and (e>g) :
+     print ("max vlue :",e)
+elif (f>e)  and (f>g) :
+     print ("max vlue :",f)
+else:
+     print ("max vlue :",g)
+
+
+
