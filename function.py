ea8faad91b877f6b945a434d8b042ec961649ce4
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-03-17 14:13:11 +0530

    Add files via upload
    python function

---
+++ function.py
@@ -0,0 +1,27 @@
+#function 
+        ##built function & pre function
+#print ("this a function")
+#print (len("radherdahe"))
+
+       #user dif function
+       #syntex function 
+ #def function_name()(parmeter1,parmeter)   
+ # ("body")
+ # ("body")
+ # return vlue
+
+def clu(a,b):
+     print ("addition :",a+b)
+     print ("substrction :",a-b)
+     print ("multipication :",a*b) 
+     print ("division :",1/2)
+
+clu(1,1)
+
+#position jis position ma sent kari a odar ma sent kare
+def kpk(fastname="gust"):
+     print ("this a name :",fastname)
+kpk("")     
+
+
+
