cc273dac59c69bf598fb55dfb4c0b265074d4799
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-03-22 22:13:14 +0530

    Update function.py

--- function.py
+++ function.py
@@ -23,5 +23,45 @@ def kpk(fastname="gust"):
      print ("this a name :",fastname)
 kpk("")     
 
+#position jis position ma sent kari a odar ma sent kare
+#def kpk(fastname="gust"):
+ #    print ("this a name :",fastname)
+#pk("")     
+
+#def test(*args):
+   # print (args)
+  #  print (len(args))
+ #   print (type(args))
+    
+#test("jj","pp","kk")
+
+#sum function
+
+def sum(a,b):
+
+    return (a+b);
+a=int(input("enter a vlue :"))
+b=int(input("enter b vlue :"))
+print (sum(a,b))
+
+#Call by reference in Python
+def tpk(l1):
+    l1.append(90);
+    l1.append(100);
+    print (l1)
+l1=[1,2,3,4,5,6]
+tpk(l1);        
+
+#ex 2
+def kkk(g1):
+    g1=g1+"hy";
+    print (g1);
+lll= "hello"
+kkk(lll)  
+print (lll);
+
+
+
+
 
 
