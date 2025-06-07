77153bd349a89d1f9e2aa1707ff2c7d8bbb14d6b
kanzariya pratik <pratikkanzariya12@gmail.com>
2025-06-07 17:41:09 +0530

    oop concepts
    class and object

---
+++ class and obect.py
@@ -0,0 +1,32 @@
+#class 
+#-->class is a blueprint for any object
+
+#object
+
+#--> obj_name=class_name()
+
+#class a:
+   # "hello" #--> print karva mate (obj.(__doc__)) thi kari sakay and class ni andar fast secotion par hovu joy
+   # def __init__(self):# --> self is defolt perameter se
+        
+    #    b=12
+     #   print (b)
+
+
+    #b=12
+    #def fun(self):
+   #     "coll by __doc__"
+  #      name="python coding"
+ #       print (name)
+
+#obj=a()
+#print (obj.fun.__doc__)
+#obj.fun()
+
+
+class a:
+    def fun(srlf,age,name,address):
+        print (age," ",name," ",address)
+
+obj=a()
+obj.fun(10,"p","kk")
